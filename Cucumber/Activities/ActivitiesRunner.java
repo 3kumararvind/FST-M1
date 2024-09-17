98343dfd6e09b64d4cf984f77b450dc164a4db58
Arvind Kumar <3kumararvind@gmail.com>
2024-09-17 13:30:36 +0530

    Update ActivitiesRunner.java

--- Cucumber/Activities/ActivitiesRunner.java
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -8,7 +8,11 @@ import org.junit.runner.RunWith;
         features = "C://Users//003E4N744//IdeaProjects//FST_Cucumber2//src//test//java//features",
         glue = {"stepDefinitions"},
         tags = "@activity7",
-        plugin = {"json: target/cucumber-reports/json-report.json"},
+        plugin= {
+                "html:src/reports/HTML_Report.html",
+               "json:src/reports/JSON_Report.json",
+                "junit:src/reports/XML_Report.xml"
+        },
         monochrome = true
 )
 public class ActivitiesRunner {
